f9d7cedb328b7522d5b7157f4a0b93d9331ce865
tungsten <osmundbeswick2@gmail.com>
2025-01-27 21:58:02 +0000

    added the venv back but put a gitignore so it wouldn't get pushed

--- poseEstimationTest.py
+++ poseEstimationTest.py
@@ -1,7 +1,14 @@
 import cv2
 import mediapipe as mp
+import matplotlib as plot
+plot.use("Qt5Agg")
+import numpy as np
 
-import matplotlib
+# Initialise matplotlib stuff
+
+figure = plot.pyplot.figure()
+axes = figure.add_subplot(111, projection="3d")
+plot.pyplot.show()
 
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
@@ -14,13 +21,15 @@ while videoCapture.isOpened:
     frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = pose.process(frameRGB)
 
-    print(results.pose_landmarks)
+    #print(results.pose_landmarks)
 
     if returned:
-        cv2.imshow("Frame", frame)
+        #cv2.imshow("Frame", frame)
 
         if cv2.waitKey(25) & 0xFF == ord("q"):
             break
 
     else:
         break
+
+    plot.pyplot.show()
