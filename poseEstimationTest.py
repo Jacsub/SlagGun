795099cfb2bb8efdc13b39bbae664d52ab305a56
tungsten <osmundbeswick2@gmail.com>
2025-01-27 23:15:02 +0000

    Got basic pose estimation done

--- poseEstimationTest.py
+++ poseEstimationTest.py
@@ -1,15 +1,10 @@
 import cv2
 import mediapipe as mp
-import matplotlib as plot
-plot.use("Qt5Agg")
 import numpy as np
 
-# Initialise matplotlib stuff
+# Computer vision/pose estimation stuff
 
-figure = plot.pyplot.figure()
-axes = figure.add_subplot(111, projection="3d")
-plot.pyplot.show()
-
+mpDraw = mp.solutions.drawing_utils
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 
@@ -21,15 +16,20 @@ while videoCapture.isOpened:
     frameRGB = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     results = pose.process(frameRGB)
 
-    #print(results.pose_landmarks)
+    poseMarks = results.pose_landmarks
+    worldMarks = results.pose_world_landmarks
+
+    if poseMarks:
+        print(poseMarks.landmark[11])
+        print(worldMarks.landmark[11])
+
+        mpDraw.draw_landmarks(frame, poseMarks)
 
     if returned:
-        #cv2.imshow("Frame", frame)
+        cv2.imshow("Frame", frame)
 
         if cv2.waitKey(25) & 0xFF == ord("q"):
             break
 
     else:
         break
-
-    plot.pyplot.show()
